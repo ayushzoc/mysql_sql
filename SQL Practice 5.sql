f1bb34d5a248b2775d0ac91f2bf15f92f0b124e4
ayushzoc <ayush168662@gmail.com>
2023-04-12 14:19:22 -0500

    Regex and Dates

--- SQL Practice 5.sql
+++ SQL Practice 5.sql
@@ -34,20 +34,20 @@ Select a.*, b.* from empinfo a inner join project b on a.id = b.student_id;
 Select a.*, b.* from empinfo a left join project b on a.id = b.student_id;
 Select * from empinfo left join project on empinfo.id = project.student_id union Select * from empinfo right join project on empinfo.id = project.student_id;
 
+Use youtube_import_export;
+Show tables;
 
+Select * from transaction_details;
+Select datediff('2023-12-04', order_date) as datediff_today, prod_name, prod_id, order_date from transaction_details;
+Select date_format(order_date, '%Y'), month(order_date), day(order_date), order_date from transaction_details;
+Select Adddate('2015-02-23', Interval 2 Month);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- REGEX
+Select * from churn_modeling;
+Select * from churn_modeling where Surname like 'a%';
+Select * from churn_modeling where Surname regexp '^a';
+# Find all the records where surname starts from a, e, i, o or u;
+# With 'like':
+Select * from churn_modeling where Surname like 'a%' or Surname like 'e%' or Surname like 'i%' or Surname like 'o%' or Surname like 'u%'; 
+# Notice the length of the statement. To keep it simple we use regex.
+Select * from churn_modeling where Surname regexp '^[aeiou]';
